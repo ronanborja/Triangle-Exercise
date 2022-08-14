65d0cfd503c9255aeef68e5d3516d660c255fb25
ronanborja <70593814+ronanborja@users.noreply.github.com>
2022-08-14 13:06:51 +0800

    Add files via upload

---
+++ TriangleVer1.java
@@ -0,0 +1,12 @@
+// Roland Nathaniel (Ronan)
+// Borja
+
+public class TriangleVer1 {
+	public static void main(String[] args) {
+		System.out.println("\t   *");
+		System.out.println("\t  ***");
+		System.out.println("\t *****");
+		System.out.println("\t*******");
+
+	}
+}
---
+++ TriangleVer2.java
@@ -0,0 +1,9 @@
+// Roland Nathaniel (Ronan)
+// Borja
+
+public class TriangleVer2 {
+	public static void main(String[] args) {
+		System.out.println("\t   * \n" + " \t  *** \n" + "\t ***** \n" + " \t*******");
+
+	}
+}
